f9f6a93be2f9933b3840f6d1fabf25a180b8c38c
rdrlaguna <rdr.laguna@gmail.com>
2023-12-01 22:34:24 +0100

    Copy input into an output file

--- ebook.py
+++ ebook.py
@@ -4,13 +4,18 @@
 #fname = input('Please enter the file name: ')
 
 # Open the file iterate over each line an print selected lines
-fhand = open('test.txt')
-count = 0
-for line in fhand:
-    line = line.rstrip()
-    if not 'D:' in line:
+source = open('test.txt', 'r')
+destination = open('out.txt', 'w')
+
+for line in source:
+    line = line
+    if 'D:' in line:
         continue
-    print(line)
+    elif '==' in line:
+        continue
+    elif '- Your Highlight' in line:
+        continue
+    destination.write(line)
 
 
 
